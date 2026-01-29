66509ada0623539e3b8727df90aed3c1fd030d46
ABuisir10 <Buisir.a@northeastern.edu>
2026-01-28 23:13:08 -0500

    Fix courses layout async params issue

--- app/(kambaz)/courses/[cid]/layout.tsx
+++ app/(kambaz)/courses/[cid]/layout.tsx
@@ -1,10 +1,15 @@
 import { ReactNode } from "react";
 import CourseNavigation from "./Navigation";
-export default async function CoursesLayout({
+
+export default function CoursesLayout({
   children,
   params,
-}: Readonly<{ children: ReactNode; params: { cid: string } }>) {
-  const { cid } = await params;
+}: {
+  children: ReactNode;
+  params: { cid: string };
+}) {
+  const { cid } = params;
+
   return (
     <div id="wd-courses">
       <h2>Courses {cid}</h2>
@@ -13,12 +18,10 @@ export default async function CoursesLayout({
         <tbody>
           <tr>
             <td valign="top" width="200">
-              {" "}
-              <CourseNavigation />{" "}
+              <CourseNavigation />
             </td>
             <td valign="top" width="100%">
-              {" "}
-              {children}{" "}
+              {children}
             </td>
           </tr>
         </tbody>
