0bcb3930e89b0a123038704ebf4522161cfdb916
ABuisir10 <Buisir.a@northeastern.edu>
2026-04-16 00:57:17 -0400

    Add missing RootState import to Navigation

--- app/(kambaz)/account/Navigation.tsx
+++ app/(kambaz)/account/Navigation.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { Nav, NavLink } from "react-bootstrap";
 import { useSelector } from "react-redux";
+import { RootState } from "../store";
 
 export default function AccountNavigation() {
   const { currentUser } = useSelector((state: RootState) => state.accountReducer);
